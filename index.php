72c4b1084549217caba70a5adce4aa03c108e44c
Ivo Petkov <ivo@ivopetkov.com>
2023-06-08 12:37:54 +0300

    Add support for the new Bear CMS Element Types API.

--- index.php
+++ index.php
@@ -26,40 +26,38 @@ $app->bearCMS->addons
                     return include $context->dir . '/locales/bg.php';
                 });
 
-            \BearCMS\Internal\ElementsTypes::add('map', [
-                'componentSrc' => 'bearcms-map-element',
-                'componentFilename' => $context->dir . '/components/mapElement.php',
-                'fields' => [
-                    [
-                        'id' => 'query',
-                        'type' => 'textbox'
-                    ],
-                    [
-                        'id' => 'latitude',
-                        'type' => 'textbox'
-                    ],
-                    [
-                        'id' => 'longitude',
-                        'type' => 'textbox'
-                    ],
-                    [
-                        'id' => 'zoom',
-                        'type' => 'textbox'
-                    ],
-                    [
-                        'id' => 'type',
-                        'type' => 'textbox'
-                    ],
-                    [
-                        'id' => 'aspectRatio',
-                        'type' => 'textbox'
-                    ],
-                    [
-                        'id' => 'height',
-                        'type' => 'textbox'
-                    ]
+            $type = new \BearCMS\Internal\ElementType('map', 'bearcms-map-element', $context->dir . '/components/mapElement.php');
+            $type->properties = [
+                [
+                    'id' => 'query',
+                    'type' => 'string'
+                ],
+                [
+                    'id' => 'latitude',
+                    'type' => 'string'
+                ],
+                [
+                    'id' => 'longitude',
+                    'type' => 'string'
+                ],
+                [
+                    'id' => 'zoom',
+                    'type' => 'string'
+                ],
+                [
+                    'id' => 'type',
+                    'type' => 'string'
+                ],
+                [
+                    'id' => 'aspectRatio',
+                    'type' => 'string'
+                ],
+                [
+                    'id' => 'height',
+                    'type' => 'string'
                 ]
-            ]);
+            ];
+            \BearCMS\Internal\ElementsTypes::add($type);
 
             \BearCMS\Internal\Themes::$elementsOptions['map'] = function ($context, $idPrefix, $parentSelector) {
                 $group = $context->addGroup(__('bearcms.themes.options.Map'));
